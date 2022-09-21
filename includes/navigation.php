bc241644741cd988d5773a3e14a0634eff42e595
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-21 14:51:34 -0500

    Create navigation.php

---
+++ includes/navigation.php
@@ -0,0 +1,13 @@
+<div class="container">
+	<ul class="nav nav-tabs">
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Page 2") {?>active<?php }?>" href="handlepost.php">Page 2</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Page 3") {?>active<?php }?>" href="page3.php">Page 3</a>
+	  </li>
+	</ul>
+</div>
