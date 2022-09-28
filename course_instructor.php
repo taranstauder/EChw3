997ab163b394e79bd284339d8ab957627e3e42a5
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-28 13:52:20 -0500

    Update course_instructor.php

--- course_instructor.php
+++ course_instructor.php
@@ -32,7 +32,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$cid = $_GET['id'];
+$iid = $_GET['id'];
 //echo $iid;
 $sql = "select course_id, description, i.instructor_name, c.prefix, c.number, i.instructor_office from course c join instructor i on i.instructor_id = c.instructor_id=" . $iid;
 //echo $sql; "select section_id, section_number, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instructor_id=" . $iid;
