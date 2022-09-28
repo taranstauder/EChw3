9274fcb9f0cd8baa90c4e07a9099623c806d5c4e
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-28 15:16:02 -0500

    Update course-cards.php

--- course-cards.php
+++ course-cards.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Students</title>
+    <title>Courses</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Students</h1>
+    <h1>Courses</h1>
 <div class="card-group">
     <?php
 $servername = "localhost";
@@ -21,8 +21,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT instructor_id, instructor_name from instructor";
+  
+$sql = "SELECT course_id, description from course";
+// DELETE $sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -31,14 +32,14 @@ if ($result->num_rows > 0) {
 ?>
    <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><?=$row["instructor_name"]?></h5>
+      <h5 class="card-title"><?=$row["description"]?></h5>
       <p class="card-text"><ul>
 <?php
-    $section_sql = "select c.description from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instructor_id=" . $row["instructor_id"];
+    $section_sql = "select i.instructor_name from take t join course c on c.course_id = t.course_id join instructor i on i.instructor_id = t.instructor_id where c.course_id=" . $row["course_id"];
     $section_result = $conn->query($section_sql);
     
     while($section_row = $section_result->fetch_assoc()) {
-      echo "<li>" . $section_row["description"] . "</li>";
+      echo "<li>" . $section_row["instructor_name"] . "</li>";
     }
 ?>
       </ul></p>
