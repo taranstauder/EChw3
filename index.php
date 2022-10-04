d8f229bb28898600edde10efca6e02a0ac8a2de4
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-10-04 13:45:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <h1>Hello!</h1>
     <p>This is my website for Homework 3. You will find tabs along the navigation bar for each table in my database. The instructor table is interactive.</p>
     <p>There is a hyperlink associated with the instructor name, this redirects you to the instructor and course tables joined.</p>
+    <p>If you click the Instructor button on the courses page it will take you to a joined record showing who instructs the course. This is done with a post variable.</p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
